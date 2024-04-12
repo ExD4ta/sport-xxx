5dd7c0474aa07946431f2ec5ad972de08927f448
ExD4ta <p13.boyko@gmail.com>
2024-04-12 10:34:50 +0200

    sito di base

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function openCategory(evt, categoryName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(categoryName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Setta il primo tab attivo all'avvio della pagina
+window.onload = function() {
+    document.getElementsByClassName('tablinks')[0].click();
+};
